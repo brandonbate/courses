753d37dc4c9362efe39555a7c4322a7b8e110a06
brandonbate <brandonbate@gmail.com>
2023-04-06 16:43:15 -0400

    Prevented from users in Students from being able to create courses. Refactored templates to inherit from base.html

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic.edit import CreateView
 
 from courses.models import Course
@@ -12,7 +12,8 @@ def index(request):
 class CourseCatalogListView(LoginRequiredMixin, ListView):
     model = Course
     
-class CreateCourseFormView(LoginRequiredMixin, CreateView):
+class CreateCourseFormView(PermissionRequiredMixin, CreateView):
     model = Course
     fields = '__all__'
+    permission_required = 'courses.course_add'
     success_url = '/'
