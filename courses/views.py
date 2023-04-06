e7715d42b435090df761416fc142a2fec2f7a36d
brandonbate <brandonbate@gmail.com>
2023-04-06 19:16:08 -0400

    Added multiple generic views that allow for easy viewing, updating and deleting of courses. The catalog is updated with links to view details. The course details includes links for updates and deletes.

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from courses.models import Course
 
@@ -16,4 +17,18 @@ class CreateCourseFormView(PermissionRequiredMixin, CreateView):
     model = Course
     fields = '__all__'
     permission_required = 'courses.course_add'
-    success_url = '/'
+    success_url = reverse_lazy('catalog')
+    
+class UpdateCourseFormView(PermissionRequiredMixin, UpdateView):
+    model = Course
+    fields = '__all__'
+    permission_required = 'courses.course_change'
+    success_url = reverse_lazy('catalog')
+
+class DeleteCourseFormView(PermissionRequiredMixin, DeleteView):
+    model = Course
+    permission_required = 'courses.course_delete'
+    success_url = reverse_lazy('catalog')
+
+class DetailCourseFormView(DetailView):
+    model = Course
