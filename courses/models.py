03dd03ce3132c6b37a23b64efeea4d7c5a39961e
brandonbate <brandonbate@gmail.com>
2023-04-06 12:32:50 -0400

    Add Course class to models.py

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Course(models.Model):
+    prefix = models.CharField(max_length=6)
+    number = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(999)])
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    credit_hours = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(24)])
